b3dbda03f73a98a17be252b1b22d1f650b8ba4b2
Kauã Gabriel <kauagabrielramkg@gmail.com>
2023-12-04 11:01:43 -0300

    'update-fabricio-produtos'

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -29,6 +29,30 @@ const services = [
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
+    {
+        title: "Remarketing",
+        description: "O remarketing em cobrança oferece uma segunda chance estratégica, reengajando devedores e otimizando a recuperação financeira.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Funil de recuperação",
+        description: "O funil de recuperação é a trajetória estruturada que guia devedores, desde o primeiro contato até a plena reabilitação financeira, maximizando a eficiência na gestão de cobranças.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Dashboard de recuperação",
+        description: "O dashboard de recuperação oferece uma visão consolidada e em tempo real das métricas essenciais, capacitando gestores a tomar decisões informadas e acelerar o processo de recuperação financeira",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Analytics",
+        description: "Analytics na gestão de cobranças proporciona insights valiosos, permitindo análises profundas para aprimorar estratégias, identificar padrões e impulsionar a eficácia na recuperação de valores.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
 ];
 
 export { services };
