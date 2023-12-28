1829f9b8734f49c3c7a83dce1a4f8cc011efb251
Kauã Gabriel <kauagabrielramkg@gmail.com>
2023-12-28 13:57:54 -0300

    'update-pontuacao'

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,31 +1,31 @@
 const services = [
     {
         title: "Chatbot",
-        description: "Solução de IA para inter-relação com clientes nos diversos canais, web, whatsapp, sms e e-mail",
+        description: "Solução de IA para inter-relação com clientes nos diversos canais, web, whatsapp, sms e e-mail.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "CRM Analytics",
-        description: "Análise das carteiras e evoluções, mapa de acompanhamento, BI e dashboard, gráficos evoluções e projeções",
+        description: "Análise das carteiras e evoluções, mapa de acompanhamento, BI e dashboard, gráficos evoluções e projeções.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Integrador",
-        description: "Conexão das soluções com plataformas, clientes e API, alertas de status e atividades, conexões com demais soluções e ajustes",
+        description: "Conexão das soluções com plataformas, clientes e API, alertas de status e atividades, conexões com demais soluções e ajustes.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Portal de cobranças",
-        description: "Soluções inteligentes para distribuição de carteiras, ativação, acompanhamento, segmentação, área logada e integração com o orquestrador",
+        description: "Soluções inteligentes para distribuição de carteiras, ativação, acompanhamento, segmentação, área logada e integração com o orquestrador.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Orquestrador",
-        description: "Solução de gestão dos canais e parâmetros de distribuição, inteligência temporal, análise de distribuição, redirecionamento, histórico de tráfego evolução e dashboard",
+        description: "Solução de gestão dos canais e parâmetros de distribuição, inteligência temporal, análise de distribuição, redirecionamento, histórico de tráfego evolução e dashboard.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
@@ -43,7 +43,7 @@ const services = [
     },
     {
         title: "Dashboard de recuperação",
-        description: "O dashboard de recuperação oferece uma visão consolidada e em tempo real das métricas essenciais, capacitando gestores a tomar decisões informadas e acelerar o processo de recuperação financeira",
+        description: "O dashboard de recuperação oferece uma visão consolidada e em tempo real das métricas essenciais, capacitando gestores a tomar decisões informadas e acelerar o processo de recuperação financeira.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
